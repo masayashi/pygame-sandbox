e1c6689c33412eacc672fbe90b560920fdf65dc4
masayashi <masayashi@gmail.com>
2017-11-19 17:11:31 +0900

    コメント

--- JustWindow.py
+++ JustWindow.py
@@ -3,19 +3,24 @@ import pygame
 from pygame.locals import QUIT
 
 pygame.init()
-SURFACE = pygame.display.set_mode( (400,300))
+
+# ディスプレイ作成
+SURFACE = pygame.display.set_mode( (1280,720) )
 pygame.display.set_caption("Just Window")
 
 def main():
-    """ main routine """
+    # メインループ
     while True:
+        # 画面クリア
         SURFACE.fill((255,255,255))
 
+        # イベントハンドル
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == QUIT: # 終了イベント
                 pygame.quit()
                 sys.exit()
 
+        # UPDATE
         pygame.display.update()
 
 if __name__ == '__main__':
